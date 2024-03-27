c7d4607827266119584de0536be6436594d963a7
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-03-26 21:29:47 -0500

    :test_tube: Add more test stubs

--- node/src/eureka.mjs
+++ node/src/eureka.mjs
@@ -1,7 +1,8 @@
+import EventEmitter from 'node:events'
 import { EurekaCrypto } from './crypto.mjs'
 import { EurekaServer } from './transport.mjs'
 
-export class Eureka {
+export class Eureka extends EventEmitter {
   constructor (props) {
 
   }
---
+++ node/src/eureka.spec.mjs
@@ -0,0 +1,11 @@
+import {ok} from 'node:assert'
+import {describe,it} from 'node:test'
+
+describe('Eureka', () => {
+    it('should emit peer-data event when peer data is processed', () => {
+        ok(false)    
+    })
+    it('should send data to peers at configured interval', () => {
+        ok(false)
+    })
+})
