41bac8f155aed2cde5b5688711bc8e20ae01f6d7
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-03-30 07:39:00 -0500

    :construction: Working transport/server class

--- node/src/transport.mjs
+++ node/src/transport.mjs
@@ -1,35 +1,113 @@
 import { EventEmitter } from 'node:events'
 import { createSocket } from 'node:dgram'
+import { networkInterfaces } from 'node:os'
 
 export class EurekaServer extends EventEmitter {
   constructor (props) {
     super()
     this.logger = props.logger
+    this.crypto = props.crypto
+    if (this.crypto == null) {
+      throw new Error('EurekaServer constructor requires props.crypto to be set with a compatible crypto class! See EurekaCrypto for compatible example')
+    }
+    this.type = props.type
+    if (this.type == null) {
+      this.type = 'udp4'
+    }
+
     this.socket = createSocket({
-      type: props.type
+      type: this.type,
+      reuseAddr: true
     })
     this.multicastGroups = props.multicastGroups
+    if (this.multicastGroups == null) {
+      // default to all on same network segment
+      this.multicastGroups = ['224.0.0.1']
+    }
     this.interfaces = props.interfaces
-    this.port = props
+    if (this.interfaces == null) {
+      this.interfaces = this.buildDefaultInterfaces()
+    } else {
+      this.validateInterfaces()
+    }
+    this.interfaceCache = networkInterfaces()
+    this.port = props.port
+    if (this.port == null) {
+      this.port = 51515
+    }
 
     // bind handlers before joining multicast groups
-      .this.socket.on('message', this.messageHandler.bind(this))
+    // bind needs to finish before we join groups
+    this.bindHandlers()
+    this.socket.bind(this.port, this.addMulticastGroups.bind(this))
+
+    // refresh the interface data every 1 minute
+    this.refreshInterfacesTimer = setInterval(this.refreshCachedInterfaces.bind(this), 60000)
+  }
+
+  getIpFamiliy () {
+    return this.type === 'udp4' ? 'ipv4' : 'ipv6'
+  }
+
+  ipFamiliyMatches (iface) {
+    const ipFam = this.getIpFamiliy()
+    return ipFam.toLowerCase() === iface.family.toLowerCase()
+  }
+
+  refreshCachedInterfaces () {
+    this.interfaceCache = networkInterfaces()
+  }
+
+  buildDefaultInterfaces () {
+    const ints = networkInterfaces()
+    const defaultInts = Object.keys(ints).filter((i) => {
+      return !ints[i][0].internal && ints[i].some((iface) => this.ipFamiliyMatches(iface))
+    })
+    return defaultInts
+  }
+
+  validateInterfaces () {
+    const nodeInterfaces = networkInterfaces()
+    for (const i in this.interfaces) {
+      if (i[nodeInterfaces] == null) throw new Error(`Interface ${i} does not exist!`)
+      if (i[nodeInterfaces][0].internal) throw new Error(`Interface ${i} is internal!`)
+    }
+  }
+
+  getIpForInterface (interfaceName) {
+    if (this.interfaceCache[interfaceName] == null) throw new Error(`Invalid interface ${interfaceName} provided in getIpForInterface`)
+    const iface = this.interfaceCache[interfaceName]
+    for (const config of iface) {
+      if (this.ipFamiliyMatches(config)) {
+        return config.address
+      }
+    }
+    throw new Error(`Interface ${interfaceName} does not have a address for ${this.type}!`)
+  }
+
+  bindHandlers () {
+    this.socket.on('message', this.messageHandler.bind(this))
     this.socket.on('error', this.messageHandler.bind(this))
     this.socket.on('listening', this.listenHandler.bind(this))
     this.socket.on('close', this.closeHandler.bind(this))
     this.socket.on('connect', this.connectHandler.bind(this))
+  }
 
-    this.socket.bind(this.port)
+  removeHandlers () {
+    this.socket.removeAllListeners()
+  }
 
-    this.multicastGroups.forEach((group) => {
+  addMulticastGroups () {
+    for (const group of this.multicastGroups) {
       if (this.interfaces) {
-        this.interfaces.forEach((i) => {
-          this.socket.addMembership(group, i)
-        })
+        for (const iface of this.interfaces) {
+          this.socket.addMembership(group, this.getIpForInterface(iface))
+        }
       } else {
         this.socket.addMembership(group)
       }
-    })
+    }
+    this.emit('ready')
   }
 
   connectHandler (msg) {
@@ -50,13 +128,33 @@ export class EurekaServer extends EventEmitter {
     }
   }
 
-  messageHandler (msg, rinfo) {
-
+  async messageHandler (msg, rinfo) {
+    try {
+      const plainText = await this.crypto.decrypt(msg, Buffer.from(`${rinfo.ip}:${rinfo.port}`))
+      this.emit('message', plainText)
+    } catch (err) {
+      this.emit(err)
+    }
   }
 
-  sendMessage (msg) {
-    for (const group in this.multicastGroups) {
-      this.socket.send(msg, this.port, group)
+  async sendMessage (msg) {
+    try {
+      for (const group of this.multicastGroups) {
+        for (const iface of this.interfaces) {
+          // to control the outgoing multicast interface we set before each call
+          // we use the interface cache to fetch the ip so we can create an appropriate context buffer for encryption
+          const ip = this.getIpForInterface(iface)
+          this.socket.setMulticastInterface(ip)
+          const encryptedMessage = await this.crypto.encrypt(msg, Buffer.from(`${ip}:${this.port}`))
+          this.socket.send(encryptedMessage, this.port, group, (err) => {
+            if (err) {
+              this.emit('error', err)
+            }
+          })
+        }
+      }
+    } catch (err) {
+      this.emit('error', err)
     }
   }
 
@@ -64,5 +162,13 @@ export class EurekaServer extends EventEmitter {
     if (this.logger) {
       this.logger.error('Server Error: ', err)
     }
+    // bubble up
+    this.emit('error', err)
+  }
+
+  closeServer () {
+    clearInterval(this.refreshInterfacesTimer)
+    this.removeHandlers()
+    this.socket.close()
   }
 }
--- node/src/transport.spec.mjs
+++ node/src/transport.spec.mjs
@@ -1,11 +1,44 @@
-import { ok } from 'node:assert'
+import { fail, ok } from 'node:assert'
 import { describe, it } from 'node:test'
+import { EurekaServer } from './transport.mjs'
 
 describe('EurekaServer', () => {
-  it('should notify listeners when data from peer is received', () => {
-    ok(false)
-  })
-  it('should send data to peer multicast group', () => {
-    ok(false)
+  it('should notify listeners when data from peer is received', async () => {
+    const identityFunc = (i) => Promise.resolve(i)
+    const testMsg = 'Hello other eureka thing!'
+    const notCryptoCrypto = {
+      encrypt: identityFunc,
+      decrypt: identityFunc
+    }
+    const errors = []
+    const instance1 = new EurekaServer({
+      crypto: notCryptoCrypto
+    }).on('ready', () => {
+      console.log('instance1 ready')
+    }).on('error', (err) => errors.push(err))
+    const instance2 = new EurekaServer({
+      crypto: notCryptoCrypto
+    }).on('ready', () => {
+      console.log('instance2 ready')
+    }).on('error', (err) => errors.push(err))
+    try {
+      const result = await new Promise((resolve, reject) => {
+        const timeout = setTimeout(reject, 10000)
+        instance2.on('message', (data) => {
+          clearTimeout(timeout)
+          resolve(data)
+        })
+        setTimeout(() => {
+          instance1.sendMessage(testMsg)
+        }, 1000)
+      })
+      ok(errors.length === 0)
+      ok(result.toString('utf8') === testMsg)
+    } catch (err) {
+      fail(err)
+    } finally {
+      instance1.closeServer()
+      instance2.closeServer()
+    }
   })
 })
