7a4baac744ae0f188915f727807deef9bde53a9b
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-04-14 23:41:31 +0000

    fix crypto export

--- node/lib.mjs
+++ node/lib.mjs
@@ -1,3 +1,3 @@
 export { EurekaServer } from './src/transport.mjs'
 export { Eureka } from './src/eureka.mjs'
-export { EurekaCrypto } from './src/eureka.mjs'
+export { EurekaCrypto } from './src/crypto.mjs'
